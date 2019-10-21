d62c2dc553d87b2de71a24a826284e623e48f560
krs <k.kamasa@gmail.com>
2019-10-21 14:45:42 +0200

    cdk

--- stack/lib/stack-stack.ts
+++ stack/lib/stack-stack.ts
@@ -2,13 +2,16 @@ import lambda = require("@aws-cdk/aws-lambda");
 import cdk = require("@aws-cdk/core");
 
 export class StackStack extends cdk.Stack {
+  public readonly lambdaCode: lambda.CfnParametersCode;
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    this.lambdaCode = lambda.Code.cfnParameters();
 
     const fn = new lambda.Function(this, "MyFunction", {
       runtime: lambda.Runtime.NODEJS_8_10,
       handler: "index.handler",
-      code: lambda.Code.asset("../lib"),
+      code: this.lambdaCode,
     });
   }
 }
