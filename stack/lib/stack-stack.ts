e00949fcfb7ee4a0c559dc53708eae4571a24b2d
krs <k.kamasa@gmail.com>
2019-10-21 15:53:20 +0200

    api

--- stack/lib/stack-stack.ts
+++ stack/lib/stack-stack.ts
@@ -4,9 +4,11 @@ import cdk = require("@aws-cdk/core");
 
 export class StackStack extends cdk.Stack {
   public readonly lambdaCode: lambda.CfnParametersCode;
+  public readonly env: cdk.CfnParameter;
 
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    this.env = new cdk.CfnParameter(this, "Env");
     this.lambdaCode = lambda.Code.cfnParameters({
       bucketNameParam: new cdk.CfnParameter(this, "CodeBucket", {
       }),
@@ -19,6 +21,10 @@ export class StackStack extends cdk.Stack {
       handler: "index.handler",
       code: this.lambdaCode,
       tracing: lambda.Tracing.ACTIVE,
+      environment: {
+        NODE_ENV: this.env.value.toString(),
+      },
+
     });
 
     const api = new apigateway.LambdaRestApi(this, "Api", {
